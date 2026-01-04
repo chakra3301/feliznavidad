748e080865fa4892bef652491cdb319a3d99e4bc
Luca Orion <lucaorion@Mac.ht.home>
2026-01-04 14:40:22 -0500

    feat: Replace KFC section with moon image and seamless fade-in effects

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -248,18 +248,18 @@ export default function Homepage() {
         </div>
       </div>
 
-      {/* KFC Image Section */}
+      {/* Moon Image Section */}
       <section className="relative w-full overflow-hidden">
-        {/* Top fade gradient */}
+        {/* Top fade gradient - seamless white fade */}
         <div className="absolute top-0 left-0 right-0 h-32 bg-gradient-to-b from-white to-transparent z-10 pointer-events-none" />
         
-        {/* Bottom fade gradient */}
+        {/* Bottom fade gradient - seamless white fade */}
         <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white to-transparent z-10 pointer-events-none" />
         
         {/* Image */}
         <img
-          src="/kfc.jpg"
-          alt="KFC"
+          src="/moon.jpg"
+          alt="Moon"
           className="w-full h-auto object-cover"
         />
       </section>
